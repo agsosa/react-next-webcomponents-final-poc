30d26f9a1403797d45cf5335b9e664339899cd3b
Alejandro Sosa <aleej.gs@gmail.com>
2025-07-21 18:01:20 -0300

    upd

--- oms/pages/_app.tsx
+++ oms/pages/_app.tsx
@@ -1,9 +1,12 @@
 import RemoteHeader from "@/components/RemoteHeader";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+    useEffect(() => {
+    document.body.classList.add("ready");
+  }, [])
   return (
     <Fragment>
       <RemoteHeader />
