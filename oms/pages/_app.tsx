56c4e666973d82411c102588e06a33225fb20b6c
Alejandro Sosa <aleej.gs@gmail.com>
2025-07-23 19:30:14 -0300

    Revert "test lifting up dynamic import"
    This reverts commit 02055f4eb5184b187a8c9588665f0563df2f0c11.

--- oms/pages/_app.tsx
+++ oms/pages/_app.tsx
@@ -1,20 +1,12 @@
 import RemoteHeader from "@/components/RemoteHeader";
-import Skeleton from "@/components/Skeleton";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import dynamic from "next/dynamic";
-
-const RemoteHeader_NOSSR = dynamic(() => Promise.resolve(RemoteHeader), {
-  ssr: false,
-  loading: () => <Skeleton style={{ width: "100%", height: "65.18px" }} />,
-});
-
 
 export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <RemoteHeader_NOSSR />
+      <RemoteHeader />
       <Component {...pageProps} />
     </>
   );
