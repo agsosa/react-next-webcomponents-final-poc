36118cf056c61994a34219d710c4c1df6e795fdc
Alejandro Sosa <aleej.gs@gmail.com>
2025-07-23 19:06:16 -0300

    Remove bumeran

--- oms/pages/_app.tsx
+++ oms/pages/_app.tsx
@@ -1,15 +1,13 @@
 import RemoteHeader from "@/components/RemoteHeader";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { DefaultTheme, ThemeProvider } from "styled-components";
-import { theme} from '@fravega-it/bumeran-ds-fvg'
 
 export default function App({ Component, pageProps }: AppProps) {
 
   return (
-    <ThemeProvider theme={theme as DefaultTheme}>
+    <>
       <RemoteHeader />
       <Component {...pageProps} />
-    </ThemeProvider>
+    </>
   );
 }
