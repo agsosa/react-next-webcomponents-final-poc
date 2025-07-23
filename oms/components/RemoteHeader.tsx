02055f4eb5184b187a8c9588665f0563df2f0c11
Alejandro Sosa <aleej.gs@gmail.com>
2025-07-23 19:20:46 -0300

    test lifting up dynamic import

--- oms/components/RemoteHeader.tsx
+++ oms/components/RemoteHeader.tsx
@@ -1,19 +1,5 @@
-import dynamic from "next/dynamic";
 import useEventListener from "../hooks/useEventListener";
 import { useState } from "react";
-import styled, { keyframes } from "styled-components";
-
-const shimmer = keyframes`
-  0% { background-position: -1000px 0; }
-  100% { background-position: 1000px 0; }
-`;
-
-export const Skeleton = styled.div`
-  background: linear-gradient(90deg, #eee 25%, #f5f5f5 50%, #eee 75%);
-  background-size: 1000px 100%;
-  animation: ${shimmer} 1.5s infinite linear;
-  border-radius: 4px;
-`;
 
 function RemoteHeader() {
   const [loaded, setLoaded] = useState(false);
@@ -30,7 +16,4 @@ function RemoteHeader() {
   );
 }
 
-export default dynamic(() => Promise.resolve(RemoteHeader), {
-  ssr: false,
-  loading: () => <Skeleton style={{ width: "100%", height: "65.18px" }} />,
-});
+export default RemoteHeader;
