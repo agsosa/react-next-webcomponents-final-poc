afc1af4112a43661dfdad0afa78896531261eca3
Alejandro Sosa <aleej.gs@gmail.com>
2025-07-28 00:21:46 -0300

    Add OMS ThemeProvider

--- oms/src/pages/_app.tsx
+++ oms/src/pages/_app.tsx
@@ -1,12 +1,19 @@
 import RemoteHeader from "@/components/RemoteHeader";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  colors: {
+    primary: 'red'
+  }
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <RemoteHeader />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
